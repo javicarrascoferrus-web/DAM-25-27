7ee410bde07740850072bf936665fef15332d88a
Javier Carrasco <javicarrascoferrus@gmail.com>
2025-12-11 04:31:27 +0100

    Update and rename 001 - PHP to 001 - Conceptos básicos.php

---
+++ Primero/Entornos de Desarrollo/008-Programacion en entorno servidor/001 - Conceptos básicos.php
@@ -0,0 +1,18 @@
+Impresión en panatlla:
+
+echo "Hola mundo";
+
+
+**Variables**
+
+No se declara el tipo y siempre llevan $ delante.
+
+PHP termina líneas con ;
+
+$nombre = "Javier";
+$edad = 20;
+
+
+Arrays (listas/diccionarios)
+
+$numeros = array(1, 2, 3);
