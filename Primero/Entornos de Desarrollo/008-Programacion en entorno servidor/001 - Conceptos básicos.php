1039360539c11beab11a2f10a8dfd57cb061f4f8
Javier Carrasco <javicarrascoferrus@gmail.com>
2025-12-11 04:33:58 +0100

    Update 001 - Conceptos básicos.php

--- Primero/Entornos de Desarrollo/008-Programacion en entorno servidor/001 - Conceptos básicos.php
+++ Primero/Entornos de Desarrollo/008-Programacion en entorno servidor/001 - Conceptos básicos.php
@@ -1,9 +1,11 @@
-Impresión en panatlla:
+<?php
+
+Impresión en pantalla:
 
 echo "Hola mundo";
 
 
-**Variables**
+Variables
 
 No se declara el tipo y siempre llevan $ delante.
 
@@ -13,6 +15,28 @@ $nombre = "Javier";
 $edad = 20;
 
 
-Arrays (listas/diccionarios)
+Arrays (listas/diccionarios):
 
 $numeros = array(1, 2, 3);
+
+Diccionario en PHP:
+
+$persona = ["nombre" => "Javier", "edad" => 20];
+
+Condiciones:
+
+if ($edad > 18) {
+    echo "Mayor";
+} else {
+    echo "Menor";
+}
+
+
+Funciones:
+
+function sumar($a, $b) {
+    return $a + $b;
+}
+
+
+
